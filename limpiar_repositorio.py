cc9bea225b4ea117e2af9f3ecd885b597e403fe7
Neil Muñoz Lago <164719485+Neiland85@users.noreply.github.com>
2024-07-12 16:56:20 +0200

    Update limpiar_repositorio.py
    Uso de pathlib en lugar de os para mejorar la legibilidad y manejabilidad del código.

--- limpiar_repositorio.py
+++ limpiar_repositorio.py
@@ -1,23 +1,14 @@
-import os
-import subprocess
+from pathlib import Path
 
-# Eliminar Archivos Innecesarios
-def eliminar_archivos_innecesarios():
-    archivos_eliminar = [".DS_Store", "__pycache__", ".pytest_cache"]
-    for root, dirs, files in os.walk('.'):
-        for archivo in archivos_eliminar:
-            if archivo in files:
-                os.remove(os.path.join(root, archivo))
-                print(f"Archivo eliminado: {os.path.join(root, archivo)}")
-            if archivo in dirs:
-                subprocess.run(['rm', '-r', os.path.join(root, archivo)])
-                print(f"Directorio eliminado: {os.path.join(root, archivo)}")
-
-def main():
-    print("Eliminando archivos innecesarios...")
-    eliminar_archivos_innecesarios()
-    print("Proceso de limpieza completado.")
+def limpiar_repositorio(directorio):
+    path = Path(directorio)
+    for file in path.rglob('*'):
+        if file.suffix in ('.pyc', '.log'):
+            try:
+                file.unlink()
+                print(f"Removed {file}")
+            except Exception as e:
+                print(f"Error removing {file}: {e}")
 
 if __name__ == "__main__":
-    main()
-
+    limpiar_repositorio('.')
