79e2dcbbfcc9c1ac2b13d764c4f0f0f735539d92
Neiland85 <bookingnadarecords@gmail.com>
2024-07-27 23:58:48 +0200

    Agregar componente Home y configurar App.js

---
+++ Projects/GitHub/Python/kurtgodelismydad.io/CiTapp/src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import Home from './components/Home';
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <Home />
+  </div>
+);
+
+export default App;
+
